1dfff2bdde5953a7697d826a97e0baa2825545be
Sayali <s@gmail.com>
2021-04-07 13:42:44 +0530

    second msg

--- src/test/java/com/selenium/VariablesExample/LoginTest.java
+++ src/test/java/com/selenium/VariablesExample/LoginTest.java
@@ -3,7 +3,13 @@ package com.selenium.VariablesExample;
 public class LoginTest {
 	
 	public static void main(String[] args) {
+		
+		
+		int i=10;
 		System.out.println("Login test");
+		
+		
+		
 		
 		
 	}
